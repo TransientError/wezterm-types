a149d1def55e7c21b60c95d825eb4dff96b8261b
kvwu <kvwu@transienterror.com>
2025-05-27 17:00:51 -0700

    spawncommand

--- types/objects/spawn-command.lua
+++ types/objects/spawn-command.lua
@@ -1,7 +1,9 @@
 ---@meta
 
---TODO: finish
-
 ---@class SpawnCommand
-local SpawnCommand = {}
-SpawnCommand.foo = "bar"
+---@field label? string The label to display in the menu.
+---@field args? string[] The command line arguments to pass to the command.
+---@field cwd? string? The working directory to use when spawning the command. If not specified, the current working directory of the terminal is used.
+---@field set_environment_variables? table<string, string> A table of environment variables to set for the command. If not specified, the current environment is used.
+---@field domain? string | table<string, string> The domain to use when spawning the command. If not specified, the default domain is used.
+---@field position? table<string, number> The position to use when spawning the command. If not specified, the default position is used.
